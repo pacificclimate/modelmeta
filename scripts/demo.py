c8f72170175e760284ee51683eb6381f77bcfd78
James Hiebert <hiebert@uvic.ca>
2013-02-21 16:59:36 -0800

    Initial implementation of modelmeta, the ORM of the pcic_meta database

---
+++ scripts/demo.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=1>
+
+# Simple example of how to use the ORM
+
+# <headingcell level=2>
+
+# Import necessary stuff
+
+# <codecell>
+
+import modelmeta
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from optparse import OptionParser
+
+# <headingcell level=2>
+
+# Create the database session
+
+# <codecell>
+
+engine = create_engine('postgresql://hiebert:mypass@windy.pcic.uvic.ca/pcic_meta?sslmode=require')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# <headingcell level=2>
+
+# Query all of some type of object
+
+# <codecell>
+
+q = session.query(modelmeta.DataFile)
+q.count()
+
+
+# <headingcell level=2>
+
+# Query related objects simply by accessing the object properties
+
+# <codecell>
+
+myfile = q[0]
+
+# <codecell>
+
+myfile.run.files[0].timeset.start_date
+
+# <codecell>
+
+myfile.run.driving_run, myfile.run.initialized_from_run
+
+# <codecell>
+
+myfile.data_file_variables[0].variable
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+import string
+from setuptools import setup
+
+__version__ = (0, 0, 1)
+
+setup(
+    name="modelmeta",
+    description="An ORM representation of the model metadata database",
+    keywords="sql database climate",
+    packages=['modelmeta'],
+    version='.'.join(str(d) for d in __version__),
+    url="http://www.pacificclimate.org/",
+    author="James Hiebert",
+    author_email="hiebert@uvic.ca",
+#    namespace_packages=['pydap', 'pydap.handlers'],
+#    entry_points='''
+#                 ''',
+#    install_requires=['pydap.handlers.sql'],
+    zip_safe=True,
+#    scripts = ['scripts/demo.py'],
+    install_requires = ['sqlalchemy', 'psycopg2'],
+        classifiers='''Development Status :: 2 - Pre-Alpha
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Software Development :: Libraries :: Python Modules'''.split('\n')
+)
