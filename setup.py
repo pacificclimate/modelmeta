fb766c0c4540b7943080d5f0f6899f3c8709243f
James Hiebert <hiebert@uvic.ca>
2014-01-21 11:43:11 -0800

    Fixed package_data paths in setup script and wrote a _simple_ test script based on the ipython demo

--- setup.py
+++ setup.py
@@ -19,15 +19,16 @@ setup(
     zip_safe=True,
 #    scripts = ['scripts/demo.py'],
     install_requires = ['sqlalchemy', 'psycopg2'],
-    package_data = {'modelmeta': 'data/mddb.sqlite'},
+    package_data = {'modelmeta': ['data/mddb-v1.sqlite', 'data/mddb-v2.sqlite']},
     include_package_data = True,
-        classifiers='''Development Status :: 2 - Pre-Alpha
-Environment :: Console
-Intended Audience :: Developers
-Intended Audience :: Science/Research
-License :: OSI Approved :: GNU General Public License (GPL)
-Operating System :: OS Independent
-Programming Language :: Python
-Topic :: Scientific/Engineering
-Topic :: Software Development :: Libraries :: Python Modules'''.split('\n')
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU General Public License (GPL)',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Software Development :: Libraries :: Python Modules'
+             ]
 )
---
+++ tests/test_mm.py
@@ -0,0 +1,52 @@
+from pkg_resources import resource_filename
+
+import modelmeta
+
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+@pytest.fixture
+def test_session():
+    f = resource_filename('modelmeta', 'data/mddb-v2.sqlite')
+    engine = create_engine('sqlite:///{0}'.format(f))
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+def test_have_data(test_session):
+    q = test_session.query(modelmeta.Ensemble)
+    assert q.count() == 1
+    assert [x.name for x in q.all()] == ['bcsd_downscale_canada']
+
+def test_data_file_vars(test_session):
+    ensemble_name = 'bcsd_downscale_canada'
+    mydatafilevars = test_session.query(modelmeta.DataFileVariable).\
+                     join(modelmeta.EnsembleDataFileVariables).\
+                     join(modelmeta.Ensemble).\
+                     filter(modelmeta.Ensemble.name == ensemble_name).all()
+    assert len(mydatafilevars) == 99
+
+def test_relations(test_session):
+    myensemble = test_session.query(modelmeta.Ensemble).filter(modelmeta.Ensemble.name == 'bcsd_downscale_canada').first()
+    assert len(myensemble.data_file_variables) == 99
+
+def test_deep_mapping(test_session):
+    myensemble = test_session.query(modelmeta.Ensemble).filter(modelmeta.Ensemble.name == 'bcsd_downscale_canada').first()
+    mymodel = myensemble.data_file_variables[0].file.run.model
+    assert mymodel.short_name == 'BCSD+ANUSPLIN300+ACCESS1-0'
+
+def test_circular_mapping(test_session):
+    myensemble = test_session.query(modelmeta.Ensemble).filter(modelmeta.Ensemble.name == 'bcsd_downscale_canada').first()
+    mymodel = myensemble.data_file_variables[0].file.run.model
+    assert mymodel.runs[0].files[0].data_file_variables[0].ensembles[0].name == 'bcsd_downscale_canada'
+
+def test_more_circles(test_session):
+
+    myensemble = test_session.query(modelmeta.Ensemble).filter(modelmeta.Ensemble.name == 'bcsd_downscale_canada').first()
+    my_filename = myensemble.data_file_variables[0].file.run.emission.\
+              runs[0].files[0].data_file_variables[0].file.filename
+    assert my_filename == '/home/data/climate/downscale/CMIP5/BCSD/pr+tasmax+tasmin_day_BCSD+ANUSPLIN300+ACCESS1-0_historical+rcp45_r1i1p1_19500101-21001231.nc'
+
+
+
