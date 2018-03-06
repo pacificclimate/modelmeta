c3a270bb37a75c3e02d9f8c6cfd7b33ed38eab33
Rod Glover <rglover@uvic.ca>
2018-03-05 17:33:36 -0800

    Add model-vs-migration schema tests

--- tests/alembic-migrations/test_migrations.py
+++ tests/alembic-migrations/test_migrations.py
@@ -5,6 +5,7 @@ from pkg_resources import resource_filename
 import pytest
 import testing.postgresql
 
+from sqlalchemy import create_engine
 from alembic import command
 from sqlalchemydiff import compare
 from sqlalchemydiff.util import (
@@ -53,3 +54,27 @@ def test_upgrade_and_downgrade(uri_left, alembic_config_left):
     while current is not None:
         command.downgrade(alembic_config_left, '-1')
         current = get_current_revision(alembic_config_left, engine, script)
+
+
+@pytest.mark.usefixtures("new_db_left")
+@pytest.mark.usefixtures("new_db_right")
+def test_model_and_migration_schemas_are_the_same(
+        uri_left, uri_right, alembic_config_left):
+    """Compare two databases.
+
+    Compares the database obtained with all migrations against the
+    one we get out of the models.
+    """
+    prepare_schema_from_migrations(uri_left, alembic_config_left)
+    engine = create_engine(uri_right)
+    engine.execute("create extension postgis")
+    prepare_schema_from_models(uri_right, Base)
+
+    result = compare(
+        uri_left, uri_right,
+        # Ignore grids.srid fkey because of the flaky way it has to be set up;
+        # for details see comments in definiton of `Grid` in `v2.py`.
+        ignores={'alembic_version', 'grids.fk.grids_srid_fkey'}
+    )
+
+    assert result.is_match
