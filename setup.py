6788bf4a2c1d661de31167dc192ce251d7a79396
James Hiebert <james@hiebert.name>
2020-07-09 11:42:44 -0700

    Drop support for < py36
    We already configured Travis to stop testing anything below Python
    3.6. This patch ensures that the Python version in the setup.py
    metadata are consistent with the tested versions.
    
    Fixes #90

--- setup.py
+++ setup.py
@@ -56,10 +56,8 @@ setup(
                  'Intended Audience :: Science/Research',
                  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                  'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  'Topic :: Scientific/Engineering',
                  'Topic :: Database',
                  'Topic :: Software Development :: Libraries :: Python Modules'
