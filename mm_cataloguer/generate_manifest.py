f199f4afe5b36e47e958e1efc8c998d216b8d76c
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-07-16 11:05:15 -0700

    Join tables to properly filter ensembles

--- mm_cataloguer/generate_manifest.py
+++ mm_cataloguer/generate_manifest.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from modelmeta import DataFile, Ensemble
+from modelmeta import DataFile, DataFileVariable, Ensemble, EnsembleDataFileVariables
 from datetime import datetime
 
 def list_filepaths(session, ensemble, since):
     if ensemble == ["all"]:
-        ensemble = session.query(Ensemble.name).all()
+        ensemble = ["all_files"]
     since = datetime.strptime(since, "%Y-%m-%d")
-    print(session.query(DataFile.filename).filter(Ensemble.name.in_(ensemble))
-                                          .filter(DataFile.index_time >= since).distinct().all())
+    print(session.query(DataFile.filename).join(DataFileVariable)
+                                          .join(EnsembleDataFileVariables)
+                                          .join(Ensemble)
+                                          .filter(Ensemble.name.in_(ensemble))
+                                          .filter(DataFile.index_time >= since).all())
 
 def Generate_Manifest(dsn, ensemble, since):
     engine = create_engine(dsn)
